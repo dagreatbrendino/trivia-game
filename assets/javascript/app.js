b82b7c8db0e6e8c0f746aadcae6815f4d037ee2a
Brendan K <brendanmkelly94@gmail.com>
2019-01-05 02:59:13 -0600

    Added timer that notifies player when their time is up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,30 +8,26 @@ var question = {
     incorrectAnswers: ["This is 1 incorrect answer", "This is 2nd incorrect answer", "This is a 3rd incorrect answer" ]
 }
 //index of correct answer
-var correctInd;
+var correctInd =0;
 //Current Question Object
-var display = {
-    //timer is greater than 0
-        //question
-        disQ: " ",
-        //array of answers
-        answers: [
-            //answer 1
-            " ",
-            //answer 2
-            " ",
-            //answer 3
-            " ",
-            //answer 4
-            " "
-        ],
-    //timer hits 0 or question has been answered
-        //result (Correct or Incorrect)
-        result: " ",
-        //if incorrect or time ran out, correct answer is displayed
-        revealedAnswer: " ",
-        //source of fun gif or pic
-        funGif: " ",
+var currentQuestion = {
+    //question
+    disQ: " ",
+    //array of answers
+    answers: [
+        //answer 1
+        " ",
+        //answer 2
+        " ",
+        //answer 3
+        " ",
+        //answer 4
+        " "
+    ],
+    //timer
+    timeleft: 10,
+    //source of fun gif or pic
+    funGif: " ",
     //Assign answer to answers array
     assignAnswer: function (ans){
         //pick a random number 0-3
@@ -45,51 +41,80 @@ var display = {
         }
     }
 }
-
-
+//function that updates the display when the correct answer is picked, and then picks a new question after a few second
+var correctAnswer = function(){
+    $("#display").empty();
+    $("#display").append("<div class = 'row'><div class='col'>Correct!</div></div>");
+}
+//function that updates the display when the incorrect answer is picked, and the picks a new question after a few seconds
+var incorrectAnswer = function(){
+    $("#display").empty();
+    $("#display").append("<div class = 'row'><div class='col'>Incorrect!</div></div>");
+    $("#display").append("<div class = 'row'><div class='col'>The correct answer was: " + currentQuestion.answers[correctInd] + "</div></div>");
+}
+//function that updates the display when the timer reaches zero
+var timeup = function(){
+    $("#display").empty();
+    $("#display").append("<div class = 'row'><div class='col'>Time is up!</div></div>");
+    $("#display").append("<div class = 'row'><div class='col'>The correct answer was: " + currentQuestion.answers[correctInd] + "</div></div>");
+}
 
 //game logic that runs timer, populates display with question and answer, and allows player to submit answer
-    display.disQ = question.q;
+    var intervalId;
+    function timer() {
+        intervalId = setInterval(decrement, 1000);
+    }
+
+    function decrement() {
+        currentQuestion.timeleft--;
+        $("#timer").html("<h4>" + currentQuestion.timeleft + "</h4>");
+        if (currentQuestion.timeleft === 0){
+            clearInterval(intervalId);
+            timeup();
+        }
+    }
+    currentQuestion.disQ = question.q;
     //assign correct answer to a slot
-    display.assignAnswer(question.correctAnswer);
+    currentQuestion.assignAnswer(question.correctAnswer);
     //assign incorrect answers to a slot
     for ( i = 0; i < 3; i++){
-        display.assignAnswer(question.incorrectAnswers[i]);
+        currentQuestion.assignAnswer(question.incorrectAnswers[i]);
     }
 
     $(document).ready(function () {
+        //Adds timer to dom
+        $("#display").append("<div class='row'><div id=timer class='col'><h4>" + currentQuestion.timeleft + "</h4></div></div>");
         //Adds question to dom
-        $("#display").append("<div class='row'><div class='col'>" + display.disQ + "</div></div>");
+        $("#display").append("<div class='row'><div class='col'>" + currentQuestion.disQ + "</div></div>");
 
         var val = 0;
         //Adds answers to dom
         for (j = 0; j < 4; j++){
             //The correct answer is given a value of 1
-            if (display.answers[j] === question.correctAnswer){
+            if (currentQuestion.answers[j] === question.correctAnswer){
                 val = 1;
+                correctInd = j;
             }
             else{
                 val = 0;
             }
-            $("#display").append("<div class='row'><div class='col'><button class='playerAns' value =" + val + ">" + display.answers[j] + "</button></div></div>");
+            $("#display").append("<div class='row'><div class='col'><button class='playerAns' value =" + val + ">" + currentQuestion.answers[j] + "</button></div></div>");
         }
         //When the player picks an answer
         $(".playerAns").on("click", function(){
             //If the answer is correct call the proper function
-            console.log(this);
-            console.log($(this).attr("value"));
-            // console.log(this.val());
-            console.log(this.value);
             if(this.value ==1){
                 console.log("correct answer picked");
+                correctAnswer();
             }
             //else the answer is incorrect call the proper function
             else{
                 console.log("incorrect answer picked");
+                incorrectAnswer();
             }
         
         })
-        
         //question timer
-            //function that will be called when the timer hits zero, it should end the users chance to answer the question and update the display
+        timer();
+            //function that will be called when the timer hits zero, it should end the users chance to answer the question and update the currentQuestion
     });
