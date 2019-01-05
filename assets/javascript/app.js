543eabd340f26bb7e0e9f37b3e6a50154306cafe
Brendan K <brendanmkelly94@gmail.com>
2019-01-05 01:16:57 -0600

    Added basic code that will grab info from question object, and add it to dom

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,71 @@
 //Question object that contains
 var question = {
     //qustion
-    
+    q: "This is a question?",
     //correct answer
+    correctAnswer: "This is the correct answer" ,
     //array of incorrect answers
+    incorrectAnswers: ["This is 1 incorrect answer", "This is 2nd incorrect answer", "This is a 3rd incorrect answer" ]
 }
-//Display object
-    //display timer
-
+//index of correct answer
+var correctInd;
+//Current Question Object
+var display = {
     //timer is greater than 0
         //question
-
-        //answer 1
-
-        //answer 2
-
-        //answer 3
-
-        //answer 4
+        disQ: " ",
+        //array of answers
+        answers: [
+            //answer 1
+            " ",
+            //answer 2
+            " ",
+            //answer 3
+            " ",
+            //answer 4
+            " "
+        ],
     //timer hits 0 or question has been answered
-        //result
+        //result (Correct or Incorrect)
+        result: " ",
+        //if incorrect or time ran out, correct answer is displayed
+        revealedAnswer: " ",
+        //source of fun gif or pic
+        funGif: " ",
+    //Assign answer to answers array
+    assignAnswer: function (ans){
+        //pick a random number 0-3
+        var index = Math.floor(Math.random()* 4);
+        //if there is no answer in the slot 
+        if (this.answers[index] === " "){
+            this.answers[index] = ans;
+        }
+        else{
+            this.assignAnswer(ans);
+        }
+    }
+}
 
-        //if in correct or time ran out, correct answer is displayed
 
-        //fun gif or pic
 
-//question timer
-    //function that will be called when the timer hits zero, it should end the users chance to answer the question and update the display
+//game logic that runs timer, populates display with question and answer, and allows player to submit answer
+    display.disQ = question.q;
+    //assign correct answer to a slot
+    display.assignAnswer(question.correctAnswer);
+    //assign incorrect answers to a slot
+    for ( i = 0; i < 3; i++){
+        display.assignAnswer(question.incorrectAnswers[i]);
+    }
+
+
+    $(document).ready(function () {
+        //Adds question to dom
+        $("#display").append("<div class='row'><div class='col'>" + display.disQ + "</div></div>");
+        //Adds answers to dom
+        for (j = 0; j < 4; j++){
+            $("#display").append("<div class='row'><div class='col'><button>" + display.answers[j] + "</button></div></div>");
+        }
+       
+        //question timer
+            //function that will be called when the timer hits zero, it should end the users chance to answer the question and update the display
+    });
