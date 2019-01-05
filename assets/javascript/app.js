d362b6e097314279c95f83dbf0b53dc084c3ced4
Brendan K <brendanmkelly94@gmail.com>
2019-01-04 23:38:20 -0600

    files created and some psuedocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,29 @@
+//Question object that contains
+var question = {
+    //qustion
+    
+    //correct answer
+    //array of incorrect answers
+}
+//Display object
+    //display timer
+
+    //timer is greater than 0
+        //question
+
+        //answer 1
+
+        //answer 2
+
+        //answer 3
+
+        //answer 4
+    //timer hits 0 or question has been answered
+        //result
+
+        //if in correct or time ran out, correct answer is displayed
+
+        //fun gif or pic
+
+//question timer
+    //function that will be called when the timer hits zero, it should end the users chance to answer the question and update the display
